129e3bfac0772ffaaa31daa13f7219315b02df85
Muh. Said Abbas <Muh. Said Abbas@LAPTOP-TUQ40I0S>
2023-01-07 21:09:42 +0700

    Project UAS Keamanan Pondok Pesantren

---
+++ src/main/java/com/mycompany/uasproject/UASProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uasproject;
+
+/**
+ *
+ * @author Muh. Said Abbas
+ */
+public class UASProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
